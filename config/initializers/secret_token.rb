c7be9597b9b3d939517964d223cc3c46ef2e484f
Ralph Bankston <ralph.l.bankston@gmail.com>
2013-09-28 11:57:57 -0400

    Initial Commit for OMRAILS using Rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = '969856a3103efc550b5cb040e8ea537c4db57cee008e8766378d454f33aaad8a84dab1aa9cff108a6d1d6afd5ea2ebb18c755181b793dc586b3cb999f10deddf'
